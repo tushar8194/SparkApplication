9ec0f711b8fcdf9172ee7f7667149ef14fd19376
tushar8194 <tushar8194@gmail.com>
2020-02-06 01:54:00 +0530

    added FlattenRDD

---
+++ ScalaSpark/src/main/scala/exampleBasics/FlattenRDD.scala
@@ -0,0 +1,27 @@
+package exampleBasics
+
+import org.apache.spark.sql.SparkSession
+
+object FlattenRDD {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().appName("Faltten-RDD").master("local").getOrCreate()
+
+    val x = spark.sparkContext.parallelize(List(("A", Set(1,2))))
+    val y = spark.sparkContext.parallelize(List(("A", Set(3,4))))
+
+    val arr = Array(x,y)
+
+    val res = arr.reduce(_ ++ _).map(x => (x._1, x._2.head, x._2.last))
+
+    res.collect().foreach(println)
+
+//    (A,1,2)
+//    (A,3,4)
+
+
+
+  }
+
+}
