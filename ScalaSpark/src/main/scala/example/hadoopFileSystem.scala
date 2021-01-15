e99fab663b5c6d8e47e102ed0732c87e5e82b5e9
tushar8194 <tushar8194@gmail.com>
2021-01-16 00:49:32 +0530

    hadoop fs API added

---
+++ ScalaSpark/src/main/scala/example/hadoopFileSystem.scala
@@ -0,0 +1,31 @@
+package example
+
+import java.io.FileNotFoundException
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+
+
+
+object hadoopFileSystem {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new Configuration()
+    val fs= FileSystem.get(conf)
+
+    val src = new Path("sourcePath")
+    val dest = new Path("destinationPath")
+
+    if(fs.exists(src))
+      fs.copyToLocalFile(false, src, dest)
+       "destinationPath"
+    else if(scala.reflect.io.File("sourcePath").exists)
+      "sourcePath"
+    else
+      new FileNotFoundException("Certificate does not exists")
+
+  }
+
+}
